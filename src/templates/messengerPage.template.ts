652e940b92e687907215d2e21c864d71f04effba
sh1nbatsu <oleg.brusnik.93@mail.ru>
2025-02-14 00:12:51 +0500

    fixed file structure

--- src/templates/messengerPage.template.ts
+++ src/templates/messengerPage.template.ts
@@ -5,11 +5,11 @@ export default `
           <a href="/profile">Profile<img src="../static/arrow_gray.svg" alt=""></a>
         </div>
         <div class="search-bar">
-          <input type="text" placeholder="Search">
+          <input type="text" placeholder="Search" name="chat_search">
           <img src="../static/search_icon.svg" alt="">
         </div>
         <div class="chat-select__wrapper">
-          {{> chatItemPartial pfpUrl="../static/mock_pfp2.jpg" chatName="Andrew" lastData="Image" lastTime="12:24" unreadAmount="1"}}
+          {{> chatItemPartial pfpUrl="../static/mock_pfp2.jpg" chatName="Andrew" lastData="Image" lastTime="12:24" unreadAmount=1}}
           {{> chatItemPartial pfpUrl="../static/mock_pfp2.jpg" chatName="Wakizashi" youSend=true lastData="Image;glkl;skl;sdfksdl;fklsdf;ksdl;fkl;sdfsdf;kl" lastTime="12:24" unreadAmount=0}}
           {{> chatItemPartial pfpUrl="../static/mock_pfp2.jpg" chatName="Wakizashi" youSend=true lastData="Image;glkl;skl;sdfksdl;fklsdf;ksdl;fkl;sdfsdf;kl" lastTime="12:24" unreadAmount="12"}}
           {{> chatItemPartial pfpUrl="../static/mock_pfp2.jpg" chatName="Wakizashi" youSend=true lastData="Image;glkl;skl;sdfksdl;fklsdf;ksdl;fkl;sdfsdf;kl" lastTime="12:24" unreadAmount="12"}}
@@ -23,8 +23,38 @@ export default `
           {{> chatItemPartial pfpUrl="../static/mock_pfp2.jpg" chatName="Wakizashi" youSend=true lastData="Image;glkl;skl;sdfksdl;fklsdf;ksdl;fkl;sdfsdf;kl" lastTime="12:24" unreadAmount="12"}}
         </div>
       </div>
-      <div class="chat__wrapper"></div>
+      <div class="chat__wrapper">
+        {{!-- <div class="placeholder__wrapper">
+          <p>
+            Select a chat to begin to send a message
+          </p>
+        </div> --}}
+        <div class="chat-main">
+          <div class="top-block">
+            <div>
+              <img src="../static/mock_pfp2.jpg" alt="">
+              <p>Onryo</p>  
+            </div>
+              <img src="../static/three_dots.svg" alt="">
+          </div>
+          <div class="messages">
+            <div class="date">19 July</div>
+            <div class="message">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, incidunt, at saepe quasi suscipit culpa amet eum cupiditate accusamus voluptate quibusdam! Quae, totam error. Maiores quae, culpa tenetur voluptate ipsum modi mollitia unde animi fuga esse impedit facere quod nisi cupiditate quos molestias dicta a corrupti iure explicabo odit. Magnam beatae at excepturi blanditiis vero obcaecati molestiae sapiente, distinctio veniam, tempore, cum nam modi ex perferendis sint. Consequatur, quaerat. Laboriosam consequatur mollitia eos cum, laborum consectetur odio sequi nulla fugiat deserunt, delectus, qui aspernatur. Autem vel debitis, iste laborum dolore laboriosam quaerat qui deleniti voluptas blanditiis, illum mollitia ex inventore sunt vero facilis eaque a! Voluptate amet, illo animi enim accusamus repellat, dolorum dolor labore maiores nobis veniam maxime. Quis, quisquam praesentium.</p>
+              <p class="message-date">13:37</p>
+            </div>
+            <div class="sent-message">
+              <p>Nice bro, very good</p>
+              <div>
+                <img src="" alt="">
+                <p></p>
+              </div>
+            </div>
+          </div>
+          <div class="bottom-block"></div>
+        </div>
+      </div>
     </div>
-`
+`;
 
-{{!-- ПОФИКСИТЬ HOVER НА CHAT ITEM PARTIAL, PADDING У NAVIG WRAPPER --}}
+// Строки 27-31 не лучшая практика, но пофиксить можно потом с помощью логики
