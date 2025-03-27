13b288cfbb13dac18c52690128d1d4c6327d0fda
sh1nbatsu <oleg.brusnik.93@mail.ru>
2025-03-28 03:32:28 +0500

    -

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,18 +1,19 @@
-// @ts-nocheck
-// @ts-ignore
-
 import { StoreEvents } from "../core/Store";
 import isEqual from "./isEqual";
-type BlockConstructor<T extends Block = Block> = new (props: unknown) => T;
 
-export function connect<P extends Record<string, unknown>>(
-  mapStateToProps: (state: unknown) => P
+// Я вообще не знаю как ЭТО  типизирвать
+// Я пытался - падают пропсы, которые я передаю при создании компонентов внутри других компонентом
+// Что делать с mixing construtor requires single argument with rest operator ...args[any] я так и не разобрался и не смог пофиксить, в конечном итоге откатился к началу
+type BlockConstructor<T extends Block = Block> = new (props: any) => T;
+
+export function connect<P extends Record<string, any>>(
+  mapStateToProps: (state: any) => P
 ) {
   return function <T extends BlockConstructor>(Component: T): T {
     return class extends Component {
       private onChangeStoreCallback: () => void;
 
-      constructor(props: unknown) {
+      constructor(props: any) {
         const store = window.store;
         let state = mapStateToProps(store.getState());
 
@@ -30,7 +31,7 @@ export function connect<P extends Record<string, unknown>>(
         store.on(StoreEvents.Updated, this.onChangeStoreCallback);
       }
 
-      setProps(nextProps: unknown) {
+      setProps(nextProps: any) {
         super.setProps(nextProps);
       }
 
