ff4f14a1c9b4035e229ef4901e45cc6124f95645
sh1nbatsu <oleg.brusnik.93@mail.ru>
2025-03-29 03:12:32 +0500

    connect.ts commentary

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -4,6 +4,10 @@ import Block from "../core/Block";
 
 type BlockConstructor<T extends Block = Block> = new (...args: any[]) => T;
 
+// Не знаю как ее исправить, так как тип any[] там необходим. Если убрать any - будет другая ошибка - a mixing class constructor should have one argument with any[] type. Тайпскрипт сам запрашивает аргумент типа any[], а линтер жалуется на него из за правила no explicit any.(насколько я понимаю)
+// https://github.com/microsoft/TypeScript/issues/37142 последний пост 28 дней назад. По всей видимости все еще не исправлено.
+// Прошу обратить внимание
+
 export function connect<P extends Record<string, unknown>>(
   mapStateToProps: (state: unknown) => P
 ) {
@@ -14,7 +18,7 @@ export function connect<P extends Record<string, unknown>>(
       constructor(...args: any[]) {
         const store = window.store;
         let stateFromStore = mapStateToProps(store.getState());
-        super({ ...(args || {}), ...stateFromStore });
+        super({ ...(args[0] || {}), ...stateFromStore });
 
         this.onChangeStoreCallback = () => {
           const newState = mapStateToProps(store.getState());
